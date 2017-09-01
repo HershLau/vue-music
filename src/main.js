fa7cb88ad8fed1b50574dfcf7a926f0c824ee5a7
hersh <neu_lhx@163.com>
2017-09-01 17:55:41 +0800

    推荐模块

--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastclick from 'fastclick'
+import VueLasyLoad from 'vue-lazyload'
 
 import 'common/stylus/index.styl'
 
 fastclick.attach(document.body)
+
+Vue.use(VueLasyLoad, {
+  loading: require('common/image/default.png')
+})
 
 /* eslint-disable no-new */
 new Vue({
