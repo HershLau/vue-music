c54dec93c0b9a946ced0ffcf7cce84b4c082cc89
hersh <neu_lhx@163.com>
2017-09-07 18:39:29 +0800

    歌单页面

--- src/components/disc/disc.vue
+++ src/components/disc/disc.vue
@@ -1,15 +1,45 @@
 <template>
   <transition name="slide">
-    <music-list></music-list>
+    <music-list :title="title" :bg-image="bgImage"></music-list>
   </transition>
 </template>
 
 <script type="text/ecmascript-6">
   import MusicList from 'components/music-list/music-list'
+  import {mapGetters} from 'vuex'
+  import {getSongList} from 'api/recommend'
+  import {ERR_OK} from 'api/config'
 
   export default {
     data() {
       return {}
+    },
+    computed: {
+      title() {
+        return this.disc.dissname
+      },
+      bgImage() {
+        return this.disc.imgurl
+      },
+      ...mapGetters([
+        'disc'
+      ])
+    },
+    created() {
+      this._getSongList()
+    },
+    methods: {
+      _getSongList() {
+        if (!this.disc.dissid) {
+          this.$router.push('/recommend')
+          return
+        }
+        getSongList(this.disc.dissid).then((res) => {
+          if (res.code === ERR_OK) {
+            console.log(res)
+          }
+        })
+      }
     },
     components: {
       MusicList
