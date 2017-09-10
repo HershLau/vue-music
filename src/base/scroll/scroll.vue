53458277e7b2294f274c01bff767b64fa93ddc44
liuhaoxuan <liuhaoxuan@star-net.cn>
2017-09-10 21:27:17 +0800

    搜索页面

--- src/base/scroll/scroll.vue
+++ src/base/scroll/scroll.vue
@@ -24,6 +24,10 @@
       listenScroll: {
         type: Boolean,
         default: false
+      },
+      pullup: {
+        type: Boolean,
+        default: false
       }
     },
     mounted() {
@@ -40,11 +44,17 @@
           probeType: this.probeType,
           click: this.click
         })
-
         if (this.listenScroll) {
           let me = this
           this.scroll.on('scroll', (pos) => {
             me.$emit('scroll', pos)
+          })
+        }
+        if (this.pullup) {
+          this.scroll.on('scrollEnd', () => {
+            if (this.scroll.y <= (this.scroll.maxScrollY + 50)) {
+              this.$emit('scrollToEnd')
+            }
           })
         }
       },
